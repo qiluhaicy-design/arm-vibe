b032d9c738827e08cfef454d5361f432ecb08e21
qiluhaicy-design <qiluhaicy@gmail.com>
2026-01-31 16:30:44 +0000

    alfa

--- drivers/virtio.h
+++ drivers/virtio.h
@@ -4,9 +4,10 @@
 #define VIRTIO_H
 
 // Define types for bare-metal
-typedef unsigned int uint32_t;
-typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 
 // Virtio device IDs
 #define VIRTIO_ID_GPU 16
