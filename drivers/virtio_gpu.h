fd13f77d6ae0b8b2578b15997f7632bab0eb0434
qiluhaicy-design <qiluhaicy@gmail.com>
2026-01-31 16:29:39 +0000

    alfa

---
+++ drivers/virtio_gpu.h
@@ -0,0 +1,10 @@
+// drivers/virtio_gpu.h - Virtio GPU header for vibeOS
+
+#ifndef VIRTIO_GPU_H
+#define VIRTIO_GPU_H
+
+// Function declarations
+void virtio_gpu_init();
+uint32_t *virtio_gpu_get_fb();
+
+#endif
